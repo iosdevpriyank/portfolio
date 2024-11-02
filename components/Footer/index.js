0010ecbaf2ead9a93ad02500ee54dd89fc403fca
Priyank Gandhi <iosdev.priyank@gmail.com>
2024-11-03 05:08:04 +0530

    Update Footer index.js
    Update Footer index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,9 +25,10 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link>
+        <a href="http://priyankgandhi.netlify.app" className="underline underline-offset-1" target="_blank" rel="noopener noreferrer">
+  Priyank Gandhi
+        </a>
+
       </h1>
     </>
   );
